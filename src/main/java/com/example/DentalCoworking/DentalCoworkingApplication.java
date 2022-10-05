1d6139357354d59d1f7ed581b717713cc62ee7aa
angel martin <angelmmgc@gmail.com>
2022-10-05 13:35:39 +0200

    añadiendo proyecto mave

---
+++ src/main/java/com/example/DentalCoworking/DentalCoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.DentalCoworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalCoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalCoworkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DentalCoworking/DentalCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DentalCoworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
