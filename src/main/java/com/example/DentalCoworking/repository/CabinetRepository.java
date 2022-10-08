3c9702a02537ba1416baca95f6580c1c05bc5cc1
angel martin <angelmmgc@gmail.com>
2022-10-08 13:06:24 +0200

    añadiendo entidades,repositorios,application.properties y .gitignore

---
+++ src/main/java/com/example/DentalCoworking/repository/CabinetRepository.java
@@ -0,0 +1,7 @@
+package com.example.DentalCoworking.repository;
+
+import com.example.DentalCoworking.entities.Cabinete;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CabinetRepository extends JpaRepository<Cabinete,Long> {
+}
