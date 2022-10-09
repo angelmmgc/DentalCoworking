31b5d0345119b34095a40f2096198ca9887e8c4b
angel martin <angelmmgc@gmail.com>
2022-10-09 21:28:48 +0200

    se añaden datos de prueba

--- src/main/java/com/example/DentalCoworking/DentalCoworkingApplication.java
+++ src/main/java/com/example/DentalCoworking/DentalCoworkingApplication.java
@@ -1,6 +1,8 @@
 package com.example.DentalCoworking;
 
+import com.example.DentalCoworking.entities.Cabinete;
 import com.example.DentalCoworking.entities.Clinic;
+import com.example.DentalCoworking.repository.CabinetRepository;
 import com.example.DentalCoworking.repository.ClinicRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +14,24 @@ public class DentalCoworkingApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext context = SpringApplication.run(DentalCoworkingApplication.class, args);
+
+		//GABINETE
+		CabinetRepository cabineteRepository = context.getBean(CabinetRepository.class);
+
+		Cabinete cabinetes = new Cabinete(null,"cirugia",false);
+
+		cabineteRepository.save(cabinetes);
+
+		//CLINICA
 		ClinicRepository clinicRepository = context.getBean(ClinicRepository.class);
+		Clinic clinica = new Clinic(null,"vital dent","952232323","vitaldent@gmail.com",3);
+		//añadimos a la clinica los gabinetes
+		clinica.getCabinete().add(cabinetes);
+		//insertamos en la base de datos clinica
+		clinicRepository.save(clinica);
 
-		Clinic clinica1 = new Clinic(null,"vital dent","952232323","vitaldent@gmail.com",3);
 
-		clinicRepository.save(clinica1);
+
 	}
 
 }
